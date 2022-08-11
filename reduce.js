27f6f18156520b23fe987157c19b8ee90995f4d9
LW83 <ljbwilkinson@gmail.com>
2022-08-11 09:35:38 +0000

    Initial commit

--- reduce.js
+++ reduce.js
@@ -6,6 +6,20 @@
 
 
 // Summing an array of numbers:
+const nums = [0, 1, 2, 3, 4];
+let sum = nums.reduce((acc, curr) => { //Console log Acc, Curr etc to see steps of how reduce works
+  console.log(
+    "Accumulator:", acc, 
+    "Current Value:", curr, 
+    "Total:", acc + curr
+  );
+  return acc + curr;
+}, 0); //0 here is specfiically saying what the accumulator should start with and the callback function will then execute 5 times(number of items in the array). Can be any number e.g. 10.
+
+//Simplified version of above
+const nums = [0, 1, 2, 3, 4];
+let sum = nums.reduce((acc, curr) => acc + curr, 0);
+console.log(sum);
 
 
 const teamMembers = [
